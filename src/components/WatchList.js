76896ccc70c42eabaa001e48372b64f6b74ca4d4
Cameron Wise <wisecameron1@gmail.com>
2021-06-29 02:13:13 -0400

    added text if wishlist is empty

--- src/components/WatchList.js
+++ src/components/WatchList.js
@@ -2,19 +2,36 @@ import React from "react";
 import { WatchListStore } from '../stores/WatchListStore';
 import {useObserver} from "mobx-react";
 import WatchListItem from './WatchListItem';
-
+import {useEffect, useState} from "react";
 
 function WatchList(){
+
+
+    const checkIfEmpty = () => {
+        if(WatchListStore.movies.length !== 0){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
     return useObserver (() =>(
         <div class = "container">
-            <div class = "movie-rows row">
-            {WatchListStore.movies.map((movie) => {
-            return <WatchListItem movie = {movie} id = {movie.id} title = {movie.title} image_link = {movie.image_link} overview = {movie.overview} onClick = {() => {
-                //possibly implement modal here
-              }}/>
-              })}
-            </div>
-          </div>
+            {
+                checkIfEmpty() ?
+                <div class = "movie-rows row">
+                    {WatchListStore.movies.map((movie) => {
+                    return <WatchListItem movie = {movie} id = {movie.id} title = {movie.title} image_link = {movie.image_link} overview = {movie.overview} onClick = {() => {
+                        //possibly implement modal here
+                    }}/>
+                    })}
+                </div> 
+                :
+                <p>your watch list is empty, add some movies by clicking on the plus icon!</p>
+            }
+            
+        </div>
     
     
 
